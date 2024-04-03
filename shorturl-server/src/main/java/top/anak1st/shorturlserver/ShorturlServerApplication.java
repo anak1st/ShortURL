ce824e08c1fbd58b136110ae3fdc945db2750195
Anak1st <xiafan8050@outlook.com>
2024-04-03 19:52:23 +0800

    init

---
+++ shorturl-server/src/main/java/top/anak1st/shorturlserver/ShorturlServerApplication.java
@@ -0,0 +1,13 @@
+package top.anak1st.shorturlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorturlServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShorturlServerApplication.class, args);
+    }
+
+}
---
+++ shorturl-server/src/test/java/top/anak1st/shorturlserver/ShorturlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.anak1st.shorturlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorturlServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
